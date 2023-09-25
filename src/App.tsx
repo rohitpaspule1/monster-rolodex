60b67ecc9998dbdace56e97a227970174c2b1ca9
rohitpaspule1 <rohitpaspule@gmail.com>
2023-09-25 11:54:34 +0530

    state basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Component } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface IState {
+name : string;
+company : string;
+}
+class App extends Component<any,IState> {
 
-  return (
+  constructor(props : any){
+    super(props);
+
+    this.state  = {
+      name : "Rohit",
+      company:"Invimatic"
+    };
+  }
+  render() {
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Hi {this.state.name}</p>
+      <button onClick={()=>{
+        // this.setState({name : "Pranit"})
+        this.setState((prev)=>{return {...prev , name : "Pranit"}},()=>{console.log(this.state)})
+      }}>Change Name</button>
     </>
-  )
+  )}
+ 
 }
 
 export default App
+
+// const obj1 = {name  : "Rohit"}
+// const obj3 = Object.assign({}, obj1)
+//setState , In this method we pass in object which we want to shallow merge with state object. 
+// state in react updates asynchronously 
+// to perform any process synchronoulsy after state is updated , use callback function
